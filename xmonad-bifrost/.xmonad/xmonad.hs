204aa837786809e0a05b2e665bbfc64d6f04ec2d
Pásztor János <model87@freemail.hu>
2020-12-07 16:25:41 +0100

    Stretch -> Buster upgrade
     * aka xmonad 0.12 -> 0.14

--- xmonad-bifrost/.xmonad/xmonad.hs
+++ xmonad-bifrost/.xmonad/xmonad.hs
@@ -22,35 +22,35 @@ import System.IO
 import qualified Data.Map as M
 
 workspaces' = ["Web", "Emacs", "Terminal", "Email", "IRC", "Local"]
-gsconfig' = defaultGSConfig { gs_cellheight = 60, gs_cellwidth = 800 }
+gsconfig' = def { gs_cellheight = 60, gs_cellwidth = 800 }
 
 main = do
   replace
   xmproc <- spawnPipe "xmobar"
-  xmonad $ ewmh defaultConfig 
+  xmonad $ ewmh def
          { modMask = mod4Mask
          , workspaces = workspaces'
          , terminal = "gnome-terminal"
          , focusFollowsMouse = True
-         , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-         , manageHook = manageDocks <+> manageHook defaultConfig
-         , layoutHook = avoidStruts $ layoutHook defaultConfig ||| noBorders Full ||| Grid (16/10)
+         , handleEventHook = handleEventHook def <+> fullscreenEventHook
+         , manageHook = manageDocks <+> manageHook def
+         , layoutHook = avoidStruts $ layoutHook def ||| noBorders Full ||| Grid (16/10)
          , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
          , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 80
-                        } >> updatePointer (0.5, 0.5) (0.5, 0.5)
+                        } >> updatePointer (0.5, 0.5) (0, 0)
 
          }
          `additionalKeysP`
          [ ("M-s", spawn "xscreensaver-command --lock")
          , ("<Scroll_lock>", spawn "pactl set-sink-mute 0 toggle")
          , ("M-p", spawn "rofi -show run")
-         , ("M-a", gridselectWorkspace defaultGSConfig W.view)
+         , ("M-a", gridselectWorkspace def W.view)
          , ("M-x", goToSelected gsconfig')
          , ("M-y", sendMessage ToggleStruts)
          , ("C-M-o", restart "/home/pasja/bin/obtoxmd" False)
-         , ("M-o", shellPrompt defaultXPConfig)
+         , ("M-o", shellPrompt def)
          , ("M-<Insert>", pasteSelection)
          , ("C-M-d", spawn "~/bin/laptop2desktop.sh")
          , ("C-M-l", spawn "~/bin/desktop2laptop.sh")
