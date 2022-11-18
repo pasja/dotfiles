4a3c9b3cc36670011e4908de4274d758fe3a87f3
Pásztor János <model87@freemail.hu>
2022-11-18 09:37:00 +0100

    New room, new monitor configuration

--- bin-bifrost/bin/laptop2desktop.sh
+++ bin-bifrost/bin/laptop2desktop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --off --output DP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-3 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --off --output DP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-3 --off
 
 xinput --set-button-map "Kensington      Kensington Expert Mouse" 1 8 3 4 5 6 7 2
